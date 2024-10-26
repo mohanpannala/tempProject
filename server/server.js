1a2f0e29c88a2fad3a21292f843b81f9839f90e8
Pannala Mohan <123240671+mohanpannala@users.noreply.github.com>
2024-10-26 13:00:57 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 dotenv.config();
