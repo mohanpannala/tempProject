4f23d0c1cdc0de78e410daf3edba1b50461ef863
Pannala Mohan <123240671+mohanpannala@users.noreply.github.com>
2024-10-26 11:49:02 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -81,6 +81,7 @@ app.post('/login', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server running on port ${port}`);
+// });
+app.listen()
