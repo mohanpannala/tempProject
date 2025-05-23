19ba59511c311ddb777554fcf7c2c1f2c6c4cf14
Pannala Mohan <123240671+mohanpannala@users.noreply.github.com>
2025-05-23 14:34:06 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "*",
+    origin: 'https://tempproject1.vercel.app',
     methods: ['POST', 'GET'],
     credentials: true
 }));
